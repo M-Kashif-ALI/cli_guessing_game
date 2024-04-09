a7613a63c37030465c86ea4745aa291c87ba7f33
M-Kashif-ALI <158289645+M-Kashif-ALI@users.noreply.github.com>
2024-04-09 10:01:55 +0500

    Game complete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import inquirer from "inquirer";
-const randomNumber = 13;
+const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number between 1 and 20:",
+        message: "Please guess a number between 1 to 10.",
     },
 ]);
 if (answers.userGuessedNumber === randomNumber) {
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import inquirer from "inquirer";
 
-const randomNumber:number = 13;
+const randomNumber = Math.floor(Math.random() * 6 + 1);
 
 const answers = await inquirer.prompt([
   {
     name: "userGuessedNumber",
     type: "number",
-    message: "Please guess a number",
+    message: "Please guess a number between 1 to 10.",
   },
 ]);
 
