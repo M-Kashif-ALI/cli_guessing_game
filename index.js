ed86755361cfffde646cc0bb6615cac3d9c14300
M-Kashif-ALI <158289645+M-Kashif-ALI@users.noreply.github.com>
2024-04-10 15:25:02 +0500

    created number guessing game

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number between 1 to 10.",
+        message: "Please guess a number between 1 to 6.",
     },
 ]);
 if (answers.userGuessedNumber === randomNumber) {
